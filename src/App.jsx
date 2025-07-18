1bcb13c4288fa1cf03a683a4c2be394b778ed869
Elvis <elvis.wachira@student.moringaschool.com>
2025-07-18 22:47:08 +0300

    Signup.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app-container">
+        <AppRoutes />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
