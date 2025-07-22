bf5bc1f4f1df8c4f57d12943082137cf1b13fd49
Elvis <elvis.wachira@student.moringaschool.com>
2025-07-23 02:06:30 +0300

    added changes on RecipeCard,routes

---
+++ src/pages/Recipes.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import RecipeCard from "../components/RecipeCard";
+import API from "../services/api.js"; // update if you named it differently
+
+const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    API.get("/recipes")
+      .then((res) => setRecipes(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="recipes-page">
+      <h1>All Recipes</h1>
+      <div className="recipe-list">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Recipes;
