8a72f830d506192725c8f2a34278a17ce5fc568b
Elvis <elvis.wachira@student.moringaschool.com>
2025-07-22 11:37:38 +0300

    created add recipe form

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import bookmarkReducer from "../feature/bookmarks/bookmarkSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer, 
+    bookmarks: bookmarkReducer,
   },
 });
 
